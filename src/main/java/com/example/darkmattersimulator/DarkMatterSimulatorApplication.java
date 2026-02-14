d00ac9c07703393f486e3cdfd8db41c99d0caee4
Devan Looches <devan.looches@gmail.com>
2026-02-13 20:26:19 -0500

    chore: Initialize project

---
+++ src/main/java/com/example/darkmattersimulator/DarkMatterSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.darkmattersimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkMatterSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkMatterSimulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/darkmattersimulator/DarkMatterSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.darkmattersimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkMatterSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
